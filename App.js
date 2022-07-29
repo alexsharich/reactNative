868759ffb25922c495fd945eab669c42b57c4ca1
alexsharich <alexsharich@gmail.com>
2022-07-29 21:13:00 +0200

    add text,buttons,images

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button ,SafeAreaView, Alert, Image, TouchableWithoutFeedback} from 'react-native';
 
 export default function App() {
+
+const handleTextPress = ()=>{
+  console.log('hello')
+}
+const handleButtonPress =()=>{
+  Alert.alert('Header','Main message',[
+    {text:'YES',onPress:()=>{console.log('Yes button')}},
+    {text:'NO',onPress:()=>{console.log('No button')}}
+  ])
+}
+const handleButtonPress2 =()=>{
+  Alert.prompt('Header','Main message',text=>console.log(text))
+}
+
   return (
-    <View style={styles.container}>
-      <Text>Alexander React Native !</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text} onPress={handleTextPress}>Alexander React Native !</Text>
+        <Button title='Click me alert' onPress={handleButtonPress} color={'red'}/>
+        <Button title='Click me prompt' onPress={handleButtonPress2} color={'green'}/>
+       <TouchableWithoutFeedback onPress={handleButtonPress}>
+       <Image blurRadius={2} style={styles.imageStyle} 
+        source={require('./assets/rn.png')}
+        />
+        {/* <Image 
+        style={styles.logo}
+        source={{
+          uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgKEh8HSajwhLGR7Tm8DZrt_-iJCf0gF9RSg&usqp=CAU",
+        }}
+        /> */}
+       </TouchableWithoutFeedback>
+        
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +44,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text:{
+    color:'red',
+  },
+  imageStyle:{
+    width:50,
+    height:50,
+  },
+  logo:{
+    width:50,
+    height:50,
   },
 });
